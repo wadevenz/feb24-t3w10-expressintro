71cf457627d3022ada853ec36ab623027b23d1a2
Wade <158378370+wadevenz@users.noreply.github.com>
2024-10-15 19:59:21 +1000

    feat: controller added and tree organised for testing

--- src/index.js
+++ src/index.js
@@ -1,33 +1,7 @@
-const express = require("express");
+const {app} = require('./server.js');
 
-// Make an instance of the Express System
-// so we can configure it. e.g routes, settings
-const app = express();
-
-app.get("/", (request, response) => {
-    // response.send("<h1>Hello world!</h1>");
-    response.json({
-        message: "Hello world!"
-    });
-});
-
-app.post("/", (request, response) => {
-    response.json({
-        message: "POST request received"
-    });
-});
-
-app.post("/bananas", (request, response) => {
-    response.json({
-        message: "POST request bananas received"
-    });
-});
-
-const PokemonController = require("./controllers/pokemonController.js");
-
-app.use("/pokemon", PokemonController);
-
-const PORT = 3000;
+// ?? = nullish coalescing
+const PORT = process.env.PORT ?? 3000;
 
 app.listen(PORT, () => {
     console.log(`Server listening on localhost:${PORT}`)
---
+++ src/server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+
+// Make an instance of the Express System
+// so we can configure it. e.g routes, settings
+const app = express();
+
+app.get("/", (request, response) => {
+    // response.send("<h1>Hello world!</h1>");
+    response.json({
+        message: "Hello world!"
+    });
+});
+
+app.post("/", (request, response) => {
+    response.json({
+        message: "POST request received"
+    });
+});
+
+app.post("/bananas", (request, response) => {
+    response.json({
+        message: "POST request bananas received"
+    });
+});
+
+const PokemonController = require("./controllers/pokemonController.js");
+
+app.use("/pokemon", PokemonController);
+
+module.exports = {
+    app
+}
